d221939d5095f2a89bdfebcd73928fe349c99d16
mrankit5575 <mrankit5575@gmail.com>
2025-11-19 12:18:53 +0530

    updat the file

---
+++ ARC_JS/sting.js
@@ -0,0 +1,24 @@
+// string add krne ke tarike 
+
+// let a = 'hello'
+// let b = ' ankit'
+// let c = a + b
+// console.log(c);
+
+// let name = 'ankit'
+// let marks = '10'
+
+// console.log(`hello ${name} your score is ${marks}`);
+// ye bhut bar kaam ane wala hai 
+
+// '' ya toh fir ""
+
+// let x = 'ankit'
+// console.log(x);
+// let y = new String(x)
+
+// console.log(y);
+
+// let y = new String("ankit")
+// console.log(y.length);
+// ouput = 5 
--- ARC_JS/test.js
+++ ARC_JS/test.js
@@ -10,7 +10,7 @@
 // console.log("8" - 5);
 // string convert kar dega number mai 
 
-// console.log("8" * 5);string convert into number
+// console.log("8" *        5);string convert into number
 
 // console.log("5" /2);
 
@@ -33,7 +33,111 @@
 
 
 
- 
+// Which variable declaration allows reassignment but not redeclaration?
+
+// var x = 32 ;
+// x = 100 ;
+// console.log(x); // 32 
+// var x = 56 ;
+// console.log(x); //56
+// log
+// toh ye nhi hai 
+
+// let x = 100 ;
+// x = 200
+// console.log(x);
+
+// let x = 300 
+// console.log(x);
+// ye answer hoga 
+
+// What is the difference between == and === operators?
+// console.log('5' == 5); ye hoga sirf value match 
+// console.log('5' === 5); ye hoga data types 
+
+// What will be the output of: 
+// console.log("10" + 2);
+// note - number change string 
+// console.log("10" - 2);
+//  note - string change number mai 
+
+
+// Which variable declaration does NOT allow reassignment?
+// var x = 56 
+// x = 100 
+// console.log(x);
+// let y = 50 ;
+// y = 69 ;
+// console.log(y);
+// ye dono toh nhi hai 
+// const z =  100 ;
+// z = 200 ;
+// console.log(z); ye galat tarika hai 
+
+// const z ;
+// z =  10 ;
+// console.log(z);
+
+// nhi kar sakte esa 
+
+// What will be the output of: 
+// console.log(a);  // TDZ 
+// let a;
+// YE ERROR SHOW KAREGA 
+
+// When doexs the default case run in a switch statement?
+// let x = "0";
+// switch (x) {
+//   case 0:
+//     console.log('off');
+    
+//      break;
+//   case 1:
+//     console.log('on');
+    
+//      break;
+//   default:
+//     console.log('no value found');
+    
+//  }
+
+// What will be printed: 
+// if("0")
+//     {console.log('True')
+
+//     } 
+// else
+// {console.log('False')
+
+// }
+
+// iksa value true hogi 
+// 0 jo hota hai output - false 
+// 1 output - true 
+
+// Which of the following are comparison operators?
+
+// = (ye use krte value dene asign krne ke liye )
+// == (compare krne ke liye not datatypes )
+// === (compare date type check krta hai )
+
+// What is the output of: 
+// console.log(typeof true);
+// boolean hoga isak ans 
+
+// Write a simple JavaScript program to check if a number is odd or even.
+
+// let num = 60505 ;
+
+// if (num % 2 == 0) {
+//     console.log('this is the even number');
+    
+    
+// } else {
+//     console.log('this is odd number');
+    
+// }
+
 
 
 
